671f90c608e6254de51a778fab5283a3ba2a1f6e
Lucas Prati <lucas@prati.fc.it>
2025-03-26 12:00:16 +0100

    Initial upload of Dice

---
+++ src/main/java/it/unibo/model/Dice/API/Dice.java
@@ -0,0 +1,5 @@
+package it.unibo.model.Dice.API;
+
+public interface Dice {
+    int throwDice();
+}
---
+++ src/main/java/it/unibo/model/Dice/impl/DiceImpl.java
@@ -0,0 +1,7 @@
+package it.unibo.model.Dice.impl;
+
+import model.*;
+
+public class DiceImpl implements Dice {
+    public int throwDice() { return 0; }
+}
