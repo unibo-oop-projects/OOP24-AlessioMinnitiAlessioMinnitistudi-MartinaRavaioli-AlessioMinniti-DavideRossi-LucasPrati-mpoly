e82e90119caeaf0a01d05dd2601abf915e6d5c5c
Lucas Prati <lucas@prati.fc.it>
2025-03-26 11:58:03 +0100

    Initial upload of BankAccount

---
+++ src/main/java/it/unibo/model/BankAccount/API/BankAccount.java
@@ -0,0 +1,6 @@
+package it.unibo.model.BankAccount.API;
+
+public interface BankAccount {
+    void deposit(int amount);
+    void withdraw(int amount);
+}
---
+++ src/main/java/it/unibo/model/BankAccount/impl/BankAccountImpl.java
@@ -0,0 +1,8 @@
+package it.unibo.model.BankAccount.impl;
+
+import model.*;
+
+public class BankAccountImpl implements BankAccount {
+    public void deposit(int amount) {}
+    public void withdraw(int amount) {}
+}
