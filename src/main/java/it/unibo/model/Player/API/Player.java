dc2dbef35583cf680a2ee05b74bf7ef64877e377
Lucas Prati <lucas.prati@studio.unibo.it>
2025-03-26 15:48:07 +0100

    fixed imports in Player

--- src/main/java/it/unibo/model/Player/API/Player.java
+++ src/main/java/it/unibo/model/Player/API/Player.java
@@ -1,5 +1,7 @@
 package it.unibo.model.Player.API;
 
+import it.unibo.model.Position.API.Position;
+
 public interface Player {
     Position getPosition();
     void makeMove(int steps);
--- src/main/java/it/unibo/model/Player/impl/PlayerImpl.java
+++ src/main/java/it/unibo/model/Player/impl/PlayerImpl.java
@@ -1,6 +1,7 @@
 package it.unibo.model.Player.impl;
 
-import model.*;
+import it.unibo.model.Player.API.Player;
+import it.unibo.model.Position.API.Position;
 
 public class PlayerImpl implements Player {
     public Position getPosition() { return null; }
