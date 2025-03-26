df8a6f1c26d61bd34e571181614029403ccffe67
Lucas Prati <lucas.prati@studio.unibo.it>
2025-03-26 15:47:06 +0100

    fixed imports in Special

--- src/main/java/it/unibo/model/Special/API/Special.java
+++ src/main/java/it/unibo/model/Special/API/Special.java
@@ -1,5 +1,8 @@
 package it.unibo.model.Special.API;
 
+import it.unibo.model.Card.API.Card;
+import it.unibo.model.Effect.API.Effect;
+
 public interface Special extends Card {
     Effect getEffect();
 }
--- src/main/java/it/unibo/model/Special/impl/SpecialImpl.java
+++ src/main/java/it/unibo/model/Special/impl/SpecialImpl.java
@@ -1,6 +1,7 @@
 package it.unibo.model.Special.impl;
 
-import model.*;
+import it.unibo.model.Effect.API.Effect;
+import it.unibo.model.Special.API.Special;
 
 public class SpecialImpl implements Special {
     public Effect getEffect() { return null; }
