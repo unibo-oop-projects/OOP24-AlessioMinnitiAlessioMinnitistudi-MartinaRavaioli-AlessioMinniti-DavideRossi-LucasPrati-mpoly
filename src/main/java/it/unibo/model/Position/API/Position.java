666a2adf6e9f681333525ca2c504d90d9464040f
Lucas Prati <lucas@prati.fc.it>
2025-03-26 11:55:14 +0100

    Initial upload of Position

---
+++ src/main/java/it/unibo/model/Position/API/Position.java
@@ -0,0 +1,3 @@
+package it.unibo.model.Position.API;
+
+public interface Position {}
---
+++ src/main/java/it/unibo/model/Position/impl/PositionImpl.java
@@ -0,0 +1,5 @@
+package it.unibo.model.Position.impl;
+
+import model.*;
+
+public class PositionImpl implements Position {}
