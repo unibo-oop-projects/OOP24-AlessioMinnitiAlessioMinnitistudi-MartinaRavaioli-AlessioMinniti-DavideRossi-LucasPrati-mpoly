24807b8634e44f044e9dbf8e1336230aaeef3d13
Lucas Prati <lucas.prati@studio.unibo.it>
2025-03-26 15:46:03 +0100

    fixed imports in TurnationManager

--- src/main/java/it/unibo/model/TurnationManager/API/TurnationManager.java
+++ src/main/java/it/unibo/model/TurnationManager/API/TurnationManager.java
@@ -1,6 +1,7 @@
 package it.unibo.model.TurnationManager.API;
 
 import it.unibo.common.Pair;
+import it.unibo.model.Player.API.Player;
 
 public interface TurnationManager {
     boolean isOver();
--- src/main/java/it/unibo/model/TurnationManager/impl/TurnationManagerImpl.java
+++ src/main/java/it/unibo/model/TurnationManager/impl/TurnationManagerImpl.java
@@ -1,7 +1,8 @@
 package it.unibo.model.TurnationManager.impl;
 
-import model.*;
-import utils.Pair;
+import it.unibo.common.Pair;
+import it.unibo.model.Player.API.Player;
+import it.unibo.model.TurnationManager.API.TurnationManager;
 
 public class TurnationManagerImpl implements TurnationManager {
     public boolean isOver() { return false; }
