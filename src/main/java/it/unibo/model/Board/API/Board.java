04dca6495c96132fb4b856bdd0e03c646d207717
Lucas Prati <lucas@prati.fc.it>
2025-03-26 11:59:25 +0100

    Initial upload of Board

---
+++ src/main/java/it/unibo/model/Board/API/Board.java
@@ -0,0 +1,8 @@
+package it.unibo.model.Board.API;
+
+public interface Board {
+    void sellHouse(Property prop);
+    void sellProperty(Property prop);
+    Card getCard(Position pos);
+    void buyProperty(Property prop, Player owner);
+}
---
+++ src/main/java/it/unibo/model/Board/impl/BoardImpl.java
@@ -0,0 +1,10 @@
+package it.unibo.model.Board.impl;
+
+import model.*;
+
+public class BoardImpl implements Board {
+    public void sellHouse(Property prop) {}
+    public void sellProperty(Property prop) {}
+    public Card getCard(Position pos) { return null; }
+    public void buyProperty(Property prop, Player owner) {}
+}
