031149266b95486019d3a51948b278ea4f5d9d94
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-03-29 15:17:52 +0100

    Add apache.commons.commons-lang3 library

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,11 @@ dependencies {
     // when dependencies share the same version, grouping in a val helps to keep them in sync
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+
+    // Apache commons lang3
+    val commonsLangVersion = "3.17.0"
+    implementation ("org.apache.commons:commons-lang3:$commonsLangVersion")
+
 }
 
 application {
