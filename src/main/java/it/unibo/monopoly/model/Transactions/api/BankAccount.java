7c4f97bffbd421abd79959d10a8b83c0f72e36c0
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-03-29 17:05:58 +0100

    add getBalance in BankAccount interface

--- src/main/java/it/unibo/monopoly/model/Transactions/api/BankAccount.java
+++ src/main/java/it/unibo/monopoly/model/Transactions/api/BankAccount.java
@@ -3,4 +3,9 @@ package it.unibo.monopoly.model.Transactions.api;
 public interface BankAccount {
     void deposit(int amount);
     void withdraw(int amount);
+
+    /**
+     * @return the current balance of the account
+     */
+    int getBalance();
 }
