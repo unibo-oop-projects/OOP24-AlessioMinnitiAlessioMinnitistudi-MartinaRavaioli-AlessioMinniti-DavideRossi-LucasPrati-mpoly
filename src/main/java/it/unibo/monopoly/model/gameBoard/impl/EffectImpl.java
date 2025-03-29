5bdd65fa7f347ea862984e83ac8ba24a1b6da1a1
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-03-29 16:04:44 +0100

    fix checkstyle EffectImpl

--- src/main/java/it/unibo/monopoly/model/gameBoard/impl/EffectImpl.java
+++ src/main/java/it/unibo/monopoly/model/gameBoard/impl/EffectImpl.java
@@ -2,4 +2,5 @@ package it.unibo.monopoly.model.gameboard.impl;
 
 import it.unibo.monopoly.model.gameboard.api.Effect;
 
-public class EffectImpl implements Effect {}
+//Effect implementation
+public class EffectImpl implements Effect { }
