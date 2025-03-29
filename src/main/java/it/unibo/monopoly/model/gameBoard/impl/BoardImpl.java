170db77065dd28db96d76efd5a2e70effa3fdbc3
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-03-29 15:56:23 +0100

    fix checkstyle BoardImpl

--- src/main/java/it/unibo/monopoly/model/gameBoard/impl/BoardImpl.java
+++ src/main/java/it/unibo/monopoly/model/gameBoard/impl/BoardImpl.java
@@ -8,17 +8,22 @@ import it.unibo.monopoly.model.gameboard.api.Tile;
 import it.unibo.monopoly.model.turnation.api.Player;
 import it.unibo.monopoly.model.turnation.api.Position;
 
+//implementation of the board
 public class BoardImpl implements Board {
     @SuppressWarnings("unused")
     private List<Tile> tiles;
-
+    //constructor
     public BoardImpl(){}
+    //sell an house
     @Override
     public void sellHouse(Property prop) {}
+    //sell the entire proprierty
     @Override
     public void sellProperty(Property prop) {}
+    //get a tile
     @Override
     public Tile getTile(Position pos) { return null; }
+    //buy a proprierty
     @Override
     public void buyProperty(Property prop, Player owner) {}
 }
