63fb749cf9c1dbd36bf1197913e50913046e4ca0
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-03-29 15:27:54 +0100

    add override annotations

--- src/main/java/it/unibo/monopoly/model/Turnation/impl/TurnationManagerImpl.java
+++ src/main/java/it/unibo/monopoly/model/Turnation/impl/TurnationManagerImpl.java
@@ -1,11 +1,15 @@
 package it.unibo.monopoly.model.turnation.impl;
 
 import org.apache.commons.lang3.tuple.Pair;
+
 import it.unibo.monopoly.model.turnation.api.Player;
 import it.unibo.monopoly.model.turnation.api.TurnationManager;
 
 public class TurnationManagerImpl implements TurnationManager {
+    @Override
     public boolean isOver() { return false; }
+    @Override
     public Player getNextPlayer() { return null; }
+    @Override
     public Pair<Integer, Integer> moveByDices() { return null; }
 }
