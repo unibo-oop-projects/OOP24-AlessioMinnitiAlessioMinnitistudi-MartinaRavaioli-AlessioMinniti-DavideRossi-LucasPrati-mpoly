ddc40cf1457e48443e6901a5cb88bfbbc7791b1c
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-03-29 16:30:37 +0100

    fix checkstyle transactions implementations

--- src/main/java/it/unibo/monopoly/model/Transactions/impl/BankAccountImpl.java
+++ src/main/java/it/unibo/monopoly/model/Transactions/impl/BankAccountImpl.java
@@ -4,7 +4,7 @@ import it.unibo.monopoly.model.transactions.api.BankAccount;
 
 public class BankAccountImpl implements BankAccount {
     @Override
-    public void deposit(int amount) {}
+    public void deposit(int amount) { }
     @Override
-    public void withdraw(int amount) {}
+    public void withdraw(int amount) { }
 }
--- src/main/java/it/unibo/monopoly/model/Transactions/impl/TransactionManagerImpl.java
+++ src/main/java/it/unibo/monopoly/model/Transactions/impl/TransactionManagerImpl.java
@@ -5,9 +5,9 @@ import it.unibo.monopoly.model.turnation.api.Player;
 
 public class TransactionManagerImpl implements TransactionManager {
     @Override
-    public void pay(Player from, Player to, int amount) {}
+    public void pay(Player from, Player to, int amount) { }
     @Override
-    public void sell(Player owner, int amount) {}
+    public void sell(Player owner, int amount) { }
     @Override
-    public void buy(Player owner, int amount) {}
+    public void buy(Player owner, int amount) { }
 }
