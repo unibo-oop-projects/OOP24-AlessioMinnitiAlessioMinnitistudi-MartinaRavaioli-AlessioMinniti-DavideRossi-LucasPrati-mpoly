620008391fb2af1ba76acc04ef9247bc3df004d0
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-03-29 16:34:13 +0100

    added turnation api's javadoc comments

--- src/main/java/it/unibo/monopoly/model/Turnation/api/TurnationManager.java
+++ src/main/java/it/unibo/monopoly/model/Turnation/api/TurnationManager.java
@@ -3,7 +3,16 @@ package it.unibo.monopoly.model.turnation.api;
 import org.apache.commons.lang3.tuple.Pair;
 
 public interface TurnationManager {
+    /**
+     * @return
+     */
     boolean isOver();
+    /**
+     * @return
+     */
     Player getNextPlayer();
+    /**
+     * @return
+     */
     Pair<Integer, Integer> moveByDices();
 }
