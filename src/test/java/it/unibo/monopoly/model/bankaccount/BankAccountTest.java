2d7c5c454753f809441387aefb69d182907b725f
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-03-29 16:43:09 +0100

    initial methods for BankAccountTest

--- src/test/java/it/unibo/monopoly/model/bankaccount/BankAccountTest.java
+++ src/test/java/it/unibo/monopoly/model/bankaccount/BankAccountTest.java
@@ -1,5 +1,49 @@
 package it.unibo.monopoly.model.bankaccount;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.monopoly.model.Transactions.api.BankAccount;
+import it.unibo.monopoly.model.Transactions.impl.BankAccountImpl;
+
 public class BankAccountTest {
+
+    private BankAccount bankAccount;
+
+
+    @BeforeEach
+    public void setUp(){
+        bankAccount = new BankAccountImpl();
+    }
+
+    @Test
+    public void checkInitialBalance() {
+        
+    }
+
+    @Test
+    public void depositPositiveAmount(){
+
+    }
+
+    @Test 
+    public void depositNegativeAmount() {
+
+    }
+
+    @Test
+    public void withdrawNegativeAmount() {
+
+    }
+
+    @Test
+    public void withdrawSufficientBalance() {
+
+    }
+
+    @Test 
+    public void withdrawWithInsufficientBalance(){
+
+    }
 
 }
