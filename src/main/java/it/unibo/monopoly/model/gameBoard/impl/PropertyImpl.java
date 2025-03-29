3730a6aaf7da21733e91c2a53e1c8ceeb5dfcec8
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-03-29 16:06:29 +0100

    fix checkstyle PropertyImpl

--- src/main/java/it/unibo/monopoly/model/gameBoard/impl/PropertyImpl.java
+++ src/main/java/it/unibo/monopoly/model/gameBoard/impl/PropertyImpl.java
@@ -4,10 +4,11 @@ import it.unibo.monopoly.model.gameboard.api.Property;
 import it.unibo.monopoly.model.turnation.api.Player;
 
 public class PropertyImpl implements Property {
-    public PropertyImpl(){}
+    //constructor
+    public PropertyImpl() { }
     //set the owner
     @Override 
-    public void setOwner(Player owner) {}
+    public void setOwner(Player owner) { }
     //get the owner
     @Override
     public Player getOwner() { return null; }
