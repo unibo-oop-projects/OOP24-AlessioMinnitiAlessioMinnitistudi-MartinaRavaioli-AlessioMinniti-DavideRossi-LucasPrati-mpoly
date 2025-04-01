a4839fa9a5ebeb9d7bce0dd9cf20aae689798d37
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-04-01 21:33:12 +0200

    getBalance and deposit methods in BankAccountImpl

--- src/main/java/it/unibo/monopoly/model/transactions/impl/BankAccountImpl.java
+++ src/main/java/it/unibo/monopoly/model/transactions/impl/BankAccountImpl.java
@@ -6,8 +6,27 @@ import it.unibo.monopoly.model.transactions.api.BankAccount;
  * bank account implementation.
 */
 public class BankAccountImpl implements BankAccount {
+
+    private static final int DEFAULT_BALANCE=0;
+    private int balance = DEFAULT_BALANCE;
+
+    public BankAccountImpl(final int initialBalance) {
+        deposit(initialBalance);
+    }
+
+
     @Override
-    public void deposit(final int amount) { }
+    public void deposit(final int amount) {
+        if(amount < 0) {
+            throw new IllegalArgumentException("Cannot receive a negative amount of money");
+        }
+
+        this.balance+=amount;
+    }
     @Override
     public void withdraw(final int amount) { }
+    @Override
+    public int getBalance() {
+        return this.balance;
+    }
 }
