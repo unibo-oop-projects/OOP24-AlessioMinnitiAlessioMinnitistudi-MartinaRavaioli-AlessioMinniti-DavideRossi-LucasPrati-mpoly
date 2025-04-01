fa0fe205bd10877e70f2bfe5470e3201b62db880
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-04-01 22:43:16 +0200

    fix style errors in BankAccount and BankAccountImpl

--- src/main/java/it/unibo/monopoly/model/transactions/api/BankAccount.java
+++ src/main/java/it/unibo/monopoly/model/transactions/api/BankAccount.java
@@ -1,16 +1,16 @@
 package it.unibo.monopoly.model.transactions.api;
 
 /**
- * bank account interface.
+ * The bank account associated with each player in the game.
 */
 public interface BankAccount {
     /**
-     * Increases the current amount of money in the account
+     * Increases the current amount of money in the account.
      * @param amount the amount of money to add to the account
      */
     void deposit(int amount);
     /** 
-     * Decreases the current amount of money in the account
+     * Decreases the current amount of money in the account.
      * @param amount the amount of money to pull from the account
      */
     void withdraw(int amount);
