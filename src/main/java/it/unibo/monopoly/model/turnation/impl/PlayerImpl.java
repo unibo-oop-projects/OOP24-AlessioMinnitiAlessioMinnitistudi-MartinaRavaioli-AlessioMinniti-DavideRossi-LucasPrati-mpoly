8451208eb826abecdf1d593758c410d33daad72f
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-07 23:20:07 +0200

    update player

--- src/main/java/it/unibo/monopoly/model/turnation/impl/PlayerImpl.java
+++ src/main/java/it/unibo/monopoly/model/turnation/impl/PlayerImpl.java
@@ -1,18 +1,12 @@
 package it.unibo.monopoly.model.turnation.impl;
 
 import it.unibo.monopoly.model.turnation.api.Player;
-import it.unibo.monopoly.model.turnation.api.Position;
 
 /**
  * player implementation.
 */
 public class PlayerImpl implements Player {
-    @Override
-    public final Position getPosition() { 
-        return null; 
-    }
-    @Override
-    public void makeMove(final int steps) { }
+    
     @Override
     public final boolean isAlive() { 
         return false; 
