2714342a8fec88639b41c07fb59d8115219c6fc3
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-07 23:25:09 +0200

    set position impl

--- src/main/java/it/unibo/monopoly/model/turnation/impl/PositionImpl.java
+++ src/main/java/it/unibo/monopoly/model/turnation/impl/PositionImpl.java
@@ -5,6 +5,18 @@ import it.unibo.monopoly.model.turnation.api.Position;
 /**
  * position implementation.
 */
-public class PositionImpl implements Position { 
+public class PositionImpl implements Position {
+
+    @Override
+    public int getPos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPos'");
+    }
+
+    @Override
+    public void setPos(int value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPos'");
+    } 
     
 }
