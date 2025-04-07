75856559ed53fcb418499a93d5a05aef084c595a
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-07 23:10:41 +0200

    created pawn impl

---
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/PawnImpl.java
@@ -0,0 +1,34 @@
+package it.unibo.monopoly.model.gameboard.impl;
+
+import it.unibo.monopoly.model.gameboard.api.Pawn;
+import it.unibo.monopoly.model.turnation.api.Position;
+import it.unibo.monopoly.model.turnation.impl.PositionImpl;
+
+public class PawnImpl implements Pawn {
+    private PositionImpl pos;
+
+    @Override
+    public Position getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public void makeMove(int steps) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makeMove'");
+    }
+
+    @Override
+    public boolean isAlive() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isAlive'");
+    }
+
+    @Override
+    public void move(int steps) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+    
+}
