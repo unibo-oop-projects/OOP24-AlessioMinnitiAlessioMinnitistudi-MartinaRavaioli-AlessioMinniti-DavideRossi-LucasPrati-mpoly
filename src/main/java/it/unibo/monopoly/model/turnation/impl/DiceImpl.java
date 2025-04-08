614119ea050f12ce4da75e0267a524c7d6c75ce9
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-08 23:36:39 +0200

    Collection dices

--- src/main/java/it/unibo/monopoly/model/turnation/impl/DiceImpl.java
+++ src/main/java/it/unibo/monopoly/model/turnation/impl/DiceImpl.java
@@ -10,13 +10,31 @@ import it.unibo.monopoly.model.turnation.api.Dice;
  * dice implementation.
 */
 public class DiceImpl implements Dice {
-    Random rand = new Random();
-    int nDices;
+    private Random rand = new Random();
+    private int nDices;
+    private int dices[];
+    private int currDice;
+
+    public DiceImpl(int n){
+        this.currDice=0;
+        setNDices(n);
+        dices=new int[this.nDices];
+    }
+
+    public DiceImpl(){
+        this.currDice=0;
+        setNDices(2);
+        dices=new int[this.nDices];
+    }
     
     @Override
     public Collection<Integer> throwDices() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'throwDices'");
+    }
+
+    private void setNDices(int value){
+        this.nDices=value;
     }
 
     @Override
