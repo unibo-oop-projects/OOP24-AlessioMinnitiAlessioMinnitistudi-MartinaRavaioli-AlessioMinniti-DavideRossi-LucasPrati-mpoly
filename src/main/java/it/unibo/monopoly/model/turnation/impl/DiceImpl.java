f96c2fea1063b63db0a199b02cb1368815347341
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-08 23:50:22 +0200

    dice impl and throw dices

--- src/main/java/it/unibo/monopoly/model/turnation/impl/DiceImpl.java
+++ src/main/java/it/unibo/monopoly/model/turnation/impl/DiceImpl.java
@@ -2,6 +2,8 @@ package it.unibo.monopoly.model.turnation.impl;
 
 import java.util.Collection;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import it.unibo.monopoly.model.turnation.api.Dice;
 
@@ -13,24 +15,23 @@ public class DiceImpl implements Dice {
     private Random rand = new Random();
     private int nDices;
     private int dices[];
-    private int currDice;
 
     public DiceImpl(int n){
-        this.currDice=0;
         setNDices(n);
         dices=new int[this.nDices];
     }
 
     public DiceImpl(){
-        this.currDice=0;
         setNDices(2);
         dices=new int[this.nDices];
     }
     
     @Override
     public Collection<Integer> throwDices() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'throwDices'");
+        return IntStream.range(0, dices.length)
+                .map(i -> rand.nextInt(5) + 1)
+                .boxed()
+                .collect(Collectors.toList());
     }
 
     private void setNDices(int value){
