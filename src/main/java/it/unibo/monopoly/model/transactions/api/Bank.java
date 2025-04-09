1ccde0f075c4129289134d070074db7b1fcff97a
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-04-09 23:07:34 +0200

    refactor methods in Bank interface

--- src/main/java/it/unibo/monopoly/model/transactions/api/Bank.java
+++ src/main/java/it/unibo/monopoly/model/transactions/api/Bank.java
@@ -1,25 +1,35 @@
 package it.unibo.monopoly.model.transactions.api;
 
+import it.unibo.monopoly.model.gameboard.api.Property;
 import it.unibo.monopoly.model.turnation.api.Player;
 
 /**
  * transaction manager interface.
 */
-public interface TransactionManager {
+public interface Bank {
+    
     /**
-     * @param from
-     * @param to
-     * @param amount
+     * Sells a property contract associated with a specific
+     * {@link BankAccount}.
+     * The property contract becomes available for purchase
+     * and the previous owner is refunded
+     * @param propertyName the name of the property to sell
      */
-    void pay(Player from, Player to, int amount);
+    void sellProperty(String propertyName);
     /**
-     * @param owner
-     * @param amount
+     * Purchase a property contract for a specifc {@link BankAccount}
+     * @param propertyName the name of the property contract to be purchased 
+     * @param newOwner the newOwner of the property
      */
-    void sell(Player owner, int amount);
+    void buyProperty(String propertyName, Player newOwner);     
+
     /**
-     * @param owner
-     * @param amount
+     * Trade an amount of money from a player's {@link BankAccount} to another.
+     * @param sender the name of the player whose {@link BankAccount} sends the money
+     * @param receiver the name of player whose {@link BankAccount} receives the money
+     * @param amount the amount of money that is exchanged
      */
-    void buy(Player owner, int amount);
+    void executePayment(String sender, String receiver, int amount);
+
+    
 }
