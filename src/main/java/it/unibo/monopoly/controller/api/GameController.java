23e0200b68df8efa446d08d68525e49f9546c932
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-04-09 22:40:49 +0200

    add game actions method signatures

--- src/main/java/it/unibo/monopoly/controller/api/GameController.java
+++ src/main/java/it/unibo/monopoly/controller/api/GameController.java
@@ -12,4 +12,31 @@ public interface GameController {
      * @param mainView the main view of the game
      */
     void addMainView(MainView mainView);
+
+    /**
+     * End the turn of the user that's currently playing and 
+     * start next player's turn.
+     * If the player cannot end its turn execution will result
+     * in an exception
+     */
+    void endTurn();
+
+    /**
+     * Throw the dices and update the position of the pawn on the gameBoard.
+     */
+    void throwDices();
+
+
+    /**
+     * Buy the property occupied by the player’s pawn
+     * whose turn it is, if the property is not owned 
+     * by any other player.
+     */
+    void buyProperty();
+
+    /**
+     * Pay the rent amount to the owner of the property
+     * occupied by the player whose turn it is.
+     */
+    void payPropertyOwner();
 }
