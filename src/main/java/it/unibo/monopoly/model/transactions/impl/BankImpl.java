a80d736316c6d76ec078db093c80c7a7be8b6de1
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-04-11 14:53:39 +0200

    create BankImpl class

---
+++ src/main/java/it/unibo/monopoly/model/transactions/impl/BankImpl.java
@@ -0,0 +1,33 @@
+package it.unibo.monopoly.model.transactions.impl;
+
+import it.unibo.monopoly.model.gameboard.api.Property;
+import it.unibo.monopoly.model.transactions.api.Bank;
+import it.unibo.monopoly.model.transactions.api.BankAccount;
+
+public class BankImpl implements Bank {
+
+    @Override
+    public void sellProperty(String propertyName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sellProperty'");
+    }
+
+    @Override
+    public void buyProperty(String propertyName, String player1Name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buyProperty'");
+    }
+
+    @Override
+    public BankAccount getBankAccount(String player1Name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBankAccount'");
+    }
+
+    @Override
+    public void payRent(String playerName, Property property) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'payRent'");
+    }
+
+}
