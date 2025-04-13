5673a4141b9e752ac755c739cdcbdf162fc274a5
Lucas Prati <lucas.prati@studio.unibo.it>
2025-04-13 23:41:09 +0200

    fixed Javadoc Comment in PlayerSetup

--- src/main/java/it/unibo/monopoly/utils/PlayerSetup.java
+++ src/main/java/it/unibo/monopoly/utils/PlayerSetup.java
@@ -3,8 +3,10 @@ package it.unibo.monopoly.utils;
 import java.awt.Color;
 
 /**
- * Player initial setup record.
- * @param name that the player chose for himself
- * @param color the color chosen for the player
+ * Represents the initial setup data for a player.
+ * Used as a Data Transfer Object (DTO) between the View and the Model.
+ *
+ * @param name  the name chosen by the player
+ * @param color the color assigned to the player
  */
 public record PlayerSetup(String name, Color color) { } 
