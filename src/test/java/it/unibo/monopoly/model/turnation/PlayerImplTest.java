7f4838f65573ecdc2222eb5fe2b4bfb7f7dfef6e
Lucas Prati <lucas.prati@studio.unibo.it>
2025-04-14 00:08:26 +0200

    fixed chechStyleError in PlayerImplTest

--- src/test/java/it/unibo/monopoly/model/turnation/PlayerImplTest.java
+++ src/test/java/it/unibo/monopoly/model/turnation/PlayerImplTest.java
@@ -1,4 +1,5 @@
 package it.unibo.monopoly.model.turnation;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
