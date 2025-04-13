3d8ddc74ff77ad21cddc12fe3f7026c2d3b6feaa
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-13 18:04:34 +0200

    abstract class tile impl

--- src/main/java/it/unibo/monopoly/model/gameboard/impl/TileImpl.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/TileImpl.java
@@ -1,7 +1,25 @@
 package it.unibo.monopoly.model.gameboard.impl;
 
 import it.unibo.monopoly.model.gameboard.api.Tile;
+import it.unibo.monopoly.model.turnation.api.Position;
+import it.unibo.monopoly.resources.Identifiable;
 
-public class TileImpl implements Tile{
+public abstract class TileImpl implements Tile,Identifiable<Position>{
+    private Position id; 
+
+    protected TileImpl(Position pos){
+        this.id=pos;
+    }
+    @Override
+    public Position getID() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getID'");
+    }
+
+    @Override
+    public void setID(Position value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setID'");
+    }
     
 }
