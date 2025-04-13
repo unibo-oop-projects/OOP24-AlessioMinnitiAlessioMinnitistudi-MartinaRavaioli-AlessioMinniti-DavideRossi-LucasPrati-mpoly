a822eb26e7d4e7095b64f008783c49828c9a26f4
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-13 17:33:51 +0200

    enum type

--- src/main/java/it/unibo/monopoly/model/gameboard/api/Tile.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/api/Tile.java
@@ -3,4 +3,15 @@ package it.unibo.monopoly.model.gameboard.api;
 /**
  * tile interface.
  */
+enum Type{
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    PURPLE,
+    ORANGE,
+    CYAN,
+    BLACK
+}
+
 public interface Tile { }
