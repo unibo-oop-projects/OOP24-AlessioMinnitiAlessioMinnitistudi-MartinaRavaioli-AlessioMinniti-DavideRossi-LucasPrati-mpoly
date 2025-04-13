0542fac05c3c47b83fb3cd9b5c49ff3b1baa47f0
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-13 17:43:26 +0200

    Tile impl

--- src/main/java/it/unibo/monopoly/model/gameboard/api/Tile.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/api/Tile.java
@@ -14,4 +14,6 @@ enum Type{
     BLACK
 }
 
-public interface Tile { }
+public interface Tile {
+
+}
---
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/TileImpl.java
@@ -0,0 +1,7 @@
+package it.unibo.monopoly.model.gameboard.impl;
+
+import it.unibo.monopoly.model.gameboard.api.Tile;
+
+public class TileImpl implements Tile{
+    
+}
