4dcd2089cadfa072dda276748400e5cb2528b62d
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-13 18:33:52 +0200

    position impl

--- src/main/java/it/unibo/monopoly/model/turnation/impl/PositionImpl.java
+++ src/main/java/it/unibo/monopoly/model/turnation/impl/PositionImpl.java
@@ -5,4 +5,16 @@ import it.unibo.monopoly.model.turnation.api.Position;
 /**
  * position implementation.
 */
-public class PositionImpl implements Position { }
+public class PositionImpl implements Position {
+    private int pos;
+
+    @Override
+    public int getPos() {
+        return this.pos;
+    }
+
+    @Override
+    public void setPos(int value) {
+        this.pos=value;
+    } 
+}
