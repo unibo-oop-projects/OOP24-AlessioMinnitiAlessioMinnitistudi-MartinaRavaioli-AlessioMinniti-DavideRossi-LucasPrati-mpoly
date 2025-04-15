9e9654e29b77d825828dc2eba4c91beb64a0a080
Lucas Prati <lucas.prati@studio.unibo.it>
2025-04-15 09:29:35 +0200

    added Javadoc comment in the interface MainMenuController

--- src/main/java/it/unibo/monopoly/controller/api/MainMenuController.java
+++ src/main/java/it/unibo/monopoly/controller/api/MainMenuController.java
@@ -17,31 +17,31 @@ public interface MainMenuController {
 
 
     /**
-     * 
+     * @implSpec decreases the field numPlayer
      */
     void decreaseNumPlayer();
 
 
     /**
-     * 
+     * @implSpec increases the field numPlayer
      */
     void increaseNumPlayer();
 
 
     /**
-     * 
+     * @return the amount of numPlayer
      */
     int getNumPlayers();
 
 
     /**
-     * 
+     * @return true if numPlayer reach the minimum, false otherwise
      */
     boolean AlreadyMinPlayers();
 
 
     /**
-     * 
+     * @return true if numPlayer reach the maximum, false otherwise
      */
     boolean AlreadyMaxPlayers();
 
