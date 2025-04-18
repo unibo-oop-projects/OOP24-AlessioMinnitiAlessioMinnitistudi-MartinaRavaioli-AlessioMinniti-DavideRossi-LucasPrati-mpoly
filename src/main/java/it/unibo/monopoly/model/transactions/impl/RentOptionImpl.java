0486a50567649b26f24c3065fa86fe144bbf4790
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-04-18 22:59:16 +0200

    add static factory method in RentOptionImpl

--- src/main/java/it/unibo/monopoly/model/transactions/impl/RentOptionImpl.java
+++ src/main/java/it/unibo/monopoly/model/transactions/impl/RentOptionImpl.java
@@ -4,6 +4,9 @@ import it.unibo.monopoly.model.transactions.api.RentOption;
 
 public record RentOptionImpl(String title,String description,int price) implements RentOption{
 
+    private static final String BASE_RENT_TITLE = "Affitto base";
+    
+
     @Override
     public String getTitle() {
        return this.title();
@@ -19,4 +22,7 @@ public record RentOptionImpl(String title,String description,int price) implemen
         return this.price();
     }
 
+    public static RentOptionImpl baseRentOption(int baseRent) {
+        return new RentOptionImpl(BASE_RENT_TITLE, "", baseRent);
+    }
 }
