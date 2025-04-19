a878ce4e0ed792dca5965698084a660e9a36f99f
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-04-19 18:28:15 +0200

    refactor methods' signatures

--- src/main/java/it/unibo/monopoly/model/transactions/api/Bank.java
+++ src/main/java/it/unibo/monopoly/model/transactions/api/Bank.java
@@ -1,37 +1,33 @@
 package it.unibo.monopoly.model.transactions.api;
 
-import it.unibo.monopoly.model.gameboard.api.Property;
-
 /**
  * transaction manager interface.
 */
 public interface Bank { 
     /**
-     * Sells a property contract associated with a specific
+     * Sells a {@link TitleDeed} associated with a specific
      * {@link BankAccount}.
-     * The property contract becomes available for purchase
+     * The {@link TitleDeed} becomes available for purchase
      * and the previous owner is refunded
-     * @param propertyName the name of the property to sell
+     * @param titleDeedName the name of the deed to sell
      */
-    void sellProperty(String propertyName);
+    void sellTitleDeed(String titleDeedName);
     /**
-     * Purchase a property contract for a specifc {@link BankAccount}.
-     * @param propertyName the name of the property contract to be purchased 
+     * Purchase a {@link TitleDeed} for a specifc player.
+     * @param titleDeedName the name of the {@link TitleDeed} to be purchased 
      * @param playerName the newOwner of the property
      */
-    void buyProperty(String propertyName, String playerName);
+    void buyTitleDeed(String titleDeedName, String playerName);
 
     /**
      * Pay the rent for stepping on a property possessed
      * by another player.
      * @param playerName the name associated to the {@link BankAccount}
      * of the player that has to pay the rent
-     * @param property the {@link Property} whose rent has to be paid,
-     * based on the state of the property the amount of money
-     * due to pay will vary as described in the {@link TitleDeed} of 
-     * the respective {@link Property}
+     * @param titleDeedName the {@link TitleDeed} whose rent has to be paid,
+     * The rent will vary based on the {@link RentOption} chose by the system
      */
-    void payRent(String playerName, Property property);
+    void payRent(String playerName, String titleDeedName);
 
 
     /**
