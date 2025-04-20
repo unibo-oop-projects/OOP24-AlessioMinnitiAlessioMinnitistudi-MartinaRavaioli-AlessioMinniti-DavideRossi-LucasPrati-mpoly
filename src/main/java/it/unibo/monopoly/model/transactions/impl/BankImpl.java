6293ae8d556fa7c742fde4695f8193b2d2418aa6
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-04-20 23:46:03 +0200

    wrote constructor for BankImpl class

--- src/main/java/it/unibo/monopoly/model/transactions/impl/BankImpl.java
+++ src/main/java/it/unibo/monopoly/model/transactions/impl/BankImpl.java
@@ -1,33 +1,77 @@
 package it.unibo.monopoly.model.transactions.impl;
 
-import it.unibo.monopoly.model.gameboard.api.Property;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+
 import it.unibo.monopoly.model.transactions.api.Bank;
 import it.unibo.monopoly.model.transactions.api.BankAccount;
+import it.unibo.monopoly.model.transactions.api.TitleDeed;
 
-public class BankImpl implements Bank {
+/**
+ * This implementation hadles all operations
+ * as described in the {@link Bank} interface. 
+ * It manages a {@code Collection} of {@link BankAccount} {@code objects}
+ * and of {@link TitleDeed} {@code objects} 
+ */
+public final class BankImpl implements Bank {
 
-    @Override
-    public void sellProperty(String propertyName) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sellProperty'");
+    private final Map<String, BankAccount> accounts;
+    private final Map<String, TitleDeed> titleDeeds;
+
+
+    /**
+     * Creates a new instance of {@link BankImpl} that 
+     * operates with the given {@code accounts} and {@code title deeds}.
+     * @param accounts the palyers' {@link BankAccount}
+     * @param titleDeeds {@link List} of {@link TitleDeed} present in the game
+     */
+    public BankImpl(final List<BankAccount> accounts, final List<TitleDeed> titleDeeds) {
+        checkForDuplicates(accounts, BankAccount::getOwner);
+        checkForDuplicates(titleDeeds, TitleDeed::getName);
+        this.accounts = Maps.uniqueIndex(accounts, BankAccount::getOwner);
+        this.titleDeeds = Maps.uniqueIndex(titleDeeds, TitleDeed::getName);
+    }
+
+    private <T, Y> void  checkForDuplicates(final List<T> list, final Function<? super T, Y> indexer) {
+        final Multimap<Y, T> index = Multimaps.index(list, indexer::apply);
+        if (index.size() < list.size()) {
+            throw new IllegalArgumentException("Duplicates were found in this list, making it unusable");
+        }
     }
 
     @Override
-    public void buyProperty(String propertyName, String player1Name) {
+    public BankAccount getBankAccount(final String playerName) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buyProperty'");
+        return null;
     }
 
     @Override
-    public BankAccount getBankAccount(String player1Name) {
+    public TitleDeed getTitleDeed(final String titleDeedName) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getBankAccount'");
+        return null;
     }
 
     @Override
-    public void payRent(String playerName, Property property) {
+    public void payRent(final String titleDeedName, final String playerName) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'payRent'");
+    }
+
+    @Override
+    public void sellTitleDeed(final String titleDeedName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sellTitleDeed'");   
+    }
+
+    @Override
+    public void buyTitleDeed(final String titleDeedName, final  String playerName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buyTitleDeed'");
     }
 
 }
