7c24d243a35121650f8c723d54c156b80e11a285
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-25 23:19:35 +0200

    create turnation manager

--- src/main/java/it/unibo/monopoly/model/turnation/impl/TurnationManagerImpl.java
+++ src/main/java/it/unibo/monopoly/model/turnation/impl/TurnationManagerImpl.java
@@ -1,5 +1,7 @@
 package it.unibo.monopoly.model.turnation.impl;
 
+import java.util.List;
+
 import org.apache.commons.lang3.tuple.Pair;
 
 import it.unibo.monopoly.model.turnation.api.Player;
@@ -9,6 +11,24 @@ import it.unibo.monopoly.model.turnation.api.TurnationManager;
  * turnation manager implementation.
 */
 public class TurnationManagerImpl implements TurnationManager {
+    private List<Player> players;
+
+    public TurnationManagerImpl(List<Player> plList){
+        this.players=plList;
+    }
+
+    public void setList(List<Player> plList){
+        this.players=plList;
+    }
+
+    public List<Player> getList(){
+        return this.players;
+    }
+
+    public void addPlayer(Player p){
+        this.players.add(p);
+    }
+
     @Override
     public final boolean isOver() { 
         return false; 
