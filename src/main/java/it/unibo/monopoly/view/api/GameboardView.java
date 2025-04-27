6d8e5014a8fce08eded2e9cd17d734158ccdabd4
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-27 17:48:15 +0200

    add pawns to board

--- src/main/java/it/unibo/monopoly/view/api/GameboardView.java
+++ src/main/java/it/unibo/monopoly/view/api/GameboardView.java
@@ -2,6 +2,7 @@ package it.unibo.monopoly.view.api;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.BorderFactory;
@@ -14,6 +15,7 @@ import it.unibo.monopoly.controller.impl.GameboardLogicImpl;
 public class GameboardView extends JFrame{
     private final GameboardLogic logic;
     private final List<JPanel> shapes=List.of(new PawnCircle(Color.RED),new PawnTriangle(Color.BLUE));
+    private final List<JPanel> tiles=new ArrayList<>();
     
     public GameboardView(int size, int players){
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -26,6 +28,7 @@ public class GameboardView extends JFrame{
         for (int i = 0; i < size; i++) {
             for(int j=0; j<size;j++){
                 JPanel tile = new JPanel();
+                tiles.add(tile);
                 
                 if (logic.isBoardTile(i,j,size)) {
                     tile.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -35,7 +38,11 @@ public class GameboardView extends JFrame{
                 }
                 board.add(tile);
             }
-            
+        }
+
+        for(int i=0; i < players; i++){
+            JPanel panel = tiles.get(i);
+            panel.add(shapes.get(i));
         }
 
         this.setVisible(true);
