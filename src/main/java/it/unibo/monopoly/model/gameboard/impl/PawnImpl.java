153d529e69d59080308d92ec84c581b5f2d0f3ab
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-04-29 21:41:35 +0200

    color and shape in pawn

--- src/main/java/it/unibo/monopoly/model/gameboard/impl/PawnImpl.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/PawnImpl.java
@@ -8,13 +8,31 @@ import it.unibo.monopoly.model.turnation.impl.AbstractIdPlayerImpl;
 import it.unibo.monopoly.model.turnation.impl.PositionImpl;
 
 public class PawnImpl extends AbstractIdPlayerImpl implements Pawn {
-    private Position pos=new PositionImpl();
+    private Position pos;
     private Color color;
     private String shape;
     
     public PawnImpl(Position id,Color color, String shape) {
         super(id.getPos());
-        this.pos=id;
+        this.pos=new PositionImpl(id.getPos());
+        setColor(color);
+        setShape(shape);
+    }
+
+    public final void setColor(Color color){
+        this.color=color;
+    }
+
+    public final void setShape(String shape){
+        this.shape=shape;
+    }
+
+    public Color getColor(){
+        return this.color;
+    }
+
+    public String getShape(){
+        return this.shape;
     }
 
     @Override
