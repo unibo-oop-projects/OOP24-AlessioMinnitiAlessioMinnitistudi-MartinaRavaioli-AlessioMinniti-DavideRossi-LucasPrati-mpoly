ecdba28bdc3df02ddd3dc6ae48f2abbce7d3691f
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-05-03 13:00:22 +0200

    add displayCurrentPlayerInfo and displayPropertyContract to MainGameView interface

--- src/main/java/it/unibo/monopoly/view/api/MainGameView.java
+++ src/main/java/it/unibo/monopoly/view/api/MainGameView.java
@@ -1,5 +1,28 @@
 package it.unibo.monopoly.view.api;
 
+import it.unibo.monopoly.model.transactions.api.BankAccount;
+import it.unibo.monopoly.model.transactions.api.TitleDeed;
+import it.unibo.monopoly.model.turnation.api.Player;
+
+/**
+ * Interface for the game view.
+ * An interface containing all methods necessary to reflect the state of the game
+ */
 public interface MainGameView {
 
+    /**
+     * Display information related to the player 
+     * that is currently playing its turn.
+     * @param plData General information of the player 
+     * @param accountData the bank account of the player
+     */
+    void displayCurrentPlayerInfo(Player plData, BankAccount accountData);
+
+    /**
+     * Display information of the {@link TitleDeed}
+     * corresponding to the {@link Property} the player's {@link Pawn}
+     * is currently on
+     * @param propertyContract the title deed to display
+     */
+    void displayPropertyContract(TitleDeed propertyContract);
 }
