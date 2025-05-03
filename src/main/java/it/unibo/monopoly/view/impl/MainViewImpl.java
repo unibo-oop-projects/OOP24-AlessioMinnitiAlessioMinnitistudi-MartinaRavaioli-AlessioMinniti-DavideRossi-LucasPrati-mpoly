e958244a81fd483716a34bfd27d50f44dde08a90
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-05-03 13:14:39 +0200

    add displayCurrentPlayerInfo method to MainViewImpl

--- src/main/java/it/unibo/monopoly/view/impl/MainViewImpl.java
+++ src/main/java/it/unibo/monopoly/view/impl/MainViewImpl.java
@@ -1,20 +1,29 @@
 package it.unibo.monopoly.view.impl;
 
+import java.util.List;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import it.unibo.monopoly.model.transactions.api.BankAccount;
+import it.unibo.monopoly.model.transactions.api.RentOptionFactory;
 import it.unibo.monopoly.model.transactions.api.TitleDeed;
 import it.unibo.monopoly.model.transactions.impl.BaseTitleDeed;
-import it.unibo.monopoly.view.api.MainView;
+import it.unibo.monopoly.model.transactions.impl.RentOptionFactoryImpl;
+import it.unibo.monopoly.model.turnation.api.Player;
+import it.unibo.monopoly.view.api.MainGameView;
 
-public class MainViewImpl implements MainView{
+public class MainViewImpl extends JFrame implements MainGameView{
 
-    private static final TitleDeed prova = new BaseTitleDeed("viola", "bastoni gran sasso", 20, s -> s / 2, 5);
-    private final JFrame mainFrame = new JFrame();
-    private final JPanel tilePanel = new ContractPanel(prova);
+    private static final RentOptionFactory fact = new RentOptionFactoryImpl();
+    private static final TitleDeed prova = new BaseTitleDeed("viola", "bastoni gran sasso", 20, s -> s / 2, 5, List.of(fact.allDeedsOfGroupWithSameOwner(5),fact.allDeedsOfGroupWithSameOwner(27)));
+    private final JPanel tilePanel = ContractPanel.createCard(prova);
 
     private MainViewImpl () {
         mainFrame.add(tilePanel);
+        /*Dimension d = new Dimension(100, 400);
+        mainFrame.setSize(d);*/
         mainFrame.pack();
         mainFrame.setVisible(true);
     }
@@ -22,5 +31,12 @@ public class MainViewImpl implements MainView{
 
     public static void main(final String[] args) {
         new MainViewImpl();
+    }
+
+
+    @Override
+    public void displayCurrentPlayerInfo(Player plData, BankAccount accountData) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayCurrentPlayerInfo'");
     }
 }
