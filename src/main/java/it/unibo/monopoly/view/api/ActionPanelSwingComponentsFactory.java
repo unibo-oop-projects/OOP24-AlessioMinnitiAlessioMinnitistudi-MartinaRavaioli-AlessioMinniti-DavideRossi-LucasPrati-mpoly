c4d8d7e15887d41d312a0b4cf5062ddad00df92a
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-05-03 14:42:33 +0200

    add ActionPanelSwingComponents factory interface

---
+++ src/main/java/it/unibo/monopoly/view/api/ActionPanelSwingComponentsFactory.java
@@ -0,0 +1,11 @@
+package it.unibo.monopoly.view.api;
+
+/**
+ * Abstract factory to create Swing components 
+ * necessary to assemble the {@link ActionPanel} UI.
+ */
+public interface ActionPanelSwingComponentsFactory {
+
+
+
+}
