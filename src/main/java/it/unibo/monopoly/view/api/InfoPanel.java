70a09b542647b50fce9672ddb0706cf7023478e6
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-05-03 23:56:38 +0200

    create interface InfoPanel

---
+++ src/main/java/it/unibo/monopoly/view/api/InfoPanel.java
@@ -0,0 +1,13 @@
+package it.unibo.monopoly.view.api;
+
+/**
+ * A panel that displays some sort of information.
+ */
+public interface InfoPanel {
+
+    /**
+     * Clear the panel content and reset it 
+     * to a default state.
+     */
+    void clear();
+}
