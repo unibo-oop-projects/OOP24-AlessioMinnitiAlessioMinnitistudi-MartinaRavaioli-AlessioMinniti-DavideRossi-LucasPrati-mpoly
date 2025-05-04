99fa082d2ad09840e87abb640267da0bb7e50131
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-05-04 12:14:23 +0200

    create class implementation of GamePanelsFactory interface

---
+++ src/main/java/it/unibo/monopoly/view/impl/ActionPanelComponentsFactoryImpl.java
@@ -0,0 +1,33 @@
+package it.unibo.monopoly.view.impl;
+
+import java.awt.Component;
+
+
+import it.unibo.monopoly.controller.api.GameController;
+import it.unibo.monopoly.view.api.GamePanelsFactory;
+import it.unibo.monopoly.view.api.PlayerPanel;
+import it.unibo.monopoly.view.api.AccountPanel;
+import it.unibo.monopoly.view.api.ContractPanel;
+
+public class ActionPanelComponentsFactoryImpl implements GamePanelsFactory{
+
+    @Override
+    public PlayerPanel userInfoPanel() {
+        return new SwingPlayerPanel();
+    }
+
+    @Override
+    public AccountPanel bankAccountInfoPanel() {
+        return new SwingAccountPanel();
+    }
+
+    @Override
+    public ContractPanel contractPanel() {
+        return new SwingContractPanel();
+    }
+
+    @Override
+    public Component mainCommandsPanel(final GameController controller) {
+        return new SwingMainCommandsPanel(controller);
+    }
+}
