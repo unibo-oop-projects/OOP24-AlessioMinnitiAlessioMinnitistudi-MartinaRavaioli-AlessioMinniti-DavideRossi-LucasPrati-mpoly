c5a0036cd57c43a994aa6e3ca95cfcead5c183f3
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-05-06 23:03:37 +0200

    add MainActionsPanel interface

---
+++ src/main/java/it/unibo/monopoly/view/api/MainActionsPanel.java
@@ -0,0 +1,17 @@
+package it.unibo.monopoly.view.api;
+
+import java.util.List;
+
+/**
+ * A panel that displays the main controls to 
+ * conduct the game.
+ */
+public interface MainActionsPanel extends InfoPanel {
+
+    /**
+     * Displays the results of the dices' throw.
+     * @param results a {@link List} of {@code Integer} containing 
+     * the numeric results of the dices.
+     */
+    void displayDicesResults(List<Integer> results);
+}
