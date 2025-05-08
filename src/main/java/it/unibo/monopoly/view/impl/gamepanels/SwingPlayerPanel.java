217f7690d7b3e1f7edfb347852fc1ec2d861fb7f
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-05-08 18:25:10 +0200

    initial implementation of class SwingPlayerPanel

--- src/main/java/it/unibo/monopoly/view/impl/gamepanels/SwingPlayerPanel.java
+++ src/main/java/it/unibo/monopoly/view/impl/gamepanels/SwingPlayerPanel.java
@@ -1,27 +1,52 @@
 package it.unibo.monopoly.view.impl.gamepanels;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import it.unibo.monopoly.model.turnation.api.Player;
 import it.unibo.monopoly.view.api.PlayerPanel;
 
-class SwingPlayerPanel extends JPanel implements PlayerPanel{
+final class SwingPlayerPanel extends JPanel implements PlayerPanel {
 
-    private static final String PLACEHOLDER = "No player selected";
+    private static final long serialVersionUID = 1L;
+    private static final String NO_PLAYER_PH = "No player selected";
+    private static final String NAME_PH = "Giocatore:";
+    //numero giocatore? 
+    private static final String PRISON_PH = "È in prigione:";
+    private static final String PRISON_TURNS_PH = "Turni rimasti da scontare:";
+    private static final String PARKING_PH = "È nel parcheggio:";
 
-    public SwingPlayerPanel() {
-        clear();
+    private final JLabel userNameJLabel = new JLabel(NAME_PH);
+    private final JLabel prisonStateJLabel = new JLabel(PRISON_PH);
+    private final JLabel prisonTurnsJLabel = new JLabel(PRISON_TURNS_PH);
+    private final JLabel parkingStateJLabel = new JLabel(PARKING_PH);
+
+    private JPanel createRow(final JLabel title, final JLabel info) {
+        title.setHorizontalAlignment(SwingConstants.LEFT);
+        info.setHorizontalAlignment(SwingConstants.RIGHT);
+        final JPanel row = new JPanel();
+        row.add(title, BorderLayout.WEST);
+        row.add(info, BorderLayout.CENTER);
+        return row;
     }
+
 
     @Override
     public void clear() {
-        final JLabel userInfoLabel = new JLabel(PLACEHOLDER);
-        this.add(userInfoLabel);
+        this.removeAll();
+        this.setLayout(new BorderLayout());
+        final JLabel userInfoLabel = new JLabel(NO_PLAYER_PH);
+        this.add(userInfoLabel, BorderLayout.CENTER);
     }
 
     @Override
-    public void displayPlayer(Player pl) {
+    public void displayPlayer(final Player pl) {
+        this.setLayout(new GridLayout(4, 1));
+        this.add(createRow(userNameJLabel, new JLabel(pl.getName())));
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'displayPlayer'");
     }
