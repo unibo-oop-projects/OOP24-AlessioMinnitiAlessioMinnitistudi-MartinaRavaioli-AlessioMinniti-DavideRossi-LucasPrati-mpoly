ceefc857cb8d23c5d9e457a7dff00348ac1e387a
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-05-08 17:44:09 +0200

    wrote ui in SwingAccountPanel class

--- src/main/java/it/unibo/monopoly/view/impl/gamepanels/SwingAccountPanel.java
+++ src/main/java/it/unibo/monopoly/view/impl/gamepanels/SwingAccountPanel.java
@@ -3,44 +3,53 @@ package it.unibo.monopoly.view.impl.gamepanels;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
-import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import it.unibo.monopoly.model.transactions.api.BankAccount;
 import it.unibo.monopoly.view.api.AccountPanel;
 
-class SwingAccountPanel extends JPanel implements AccountPanel{
+final class SwingAccountPanel extends JPanel implements AccountPanel {
 
-    private static final String DESC = "Saldo";
-    private static final String PLACEHOLDER = "None";
+    private static final String BALANCE = "Saldo:";
+    private static final String BL_PLACEHOLDER = "No balance";
+    private static final long serialVersionUID = 1L;
 
-    public SwingAccountPanel() {
+
+    private final JLabel balanceJLabel;
+
+    SwingAccountPanel() {
+        final GridBagLayout balancePanelLayout = new GridBagLayout();
+        this.setLayout(balancePanelLayout);
+
+        final JLabel balanceTitleLabel = new JLabel(BALANCE);
+        balanceJLabel = new JLabel();
+        balanceJLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        this.add(balanceTitleLabel);
+        this.add(balanceJLabel);
+
+        final GridBagConstraints balanceTitleConstraints = new GridBagConstraints();
+        balanceTitleConstraints.fill = GridBagConstraints.BOTH;
+        balanceTitleConstraints.weighty = 1.0;
+        balancePanelLayout.setConstraints(balanceTitleLabel, balanceTitleConstraints);
+
+        final GridBagConstraints balanceLabelConstraints = new GridBagConstraints();
+        balanceLabelConstraints.weighty = 1.0;
+        balanceLabelConstraints.weightx = 1.0;
+        balanceLabelConstraints.fill = GridBagConstraints.BOTH;
+        balanceLabelConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        balancePanelLayout.setConstraints(balanceJLabel, balanceLabelConstraints);
         clear();
     }
 
     @Override
     public void clear() {
-        final GridBagLayout balancePanelLayout = new GridBagLayout();
-        this.setLayout(balancePanelLayout);
-
-        final JButton balanceTitleLabel = new JButton(DESC);
-        final JButton balanceLabel = new JButton(PLACEHOLDER);
-        this.add(balanceTitleLabel);
-        this.add(balanceLabel);
-
-        final GridBagConstraints balanceTitleConstraints = new GridBagConstraints();                
-        balancePanelLayout.setConstraints(balanceTitleLabel, balanceTitleConstraints);
-
-        final GridBagConstraints balanceLabelConstraints = new GridBagConstraints();
-        balanceLabelConstraints.weightx = 1.0;
-        balanceLabelConstraints.fill = GridBagConstraints.HORIZONTAL;
-        balancePanelLayout.setConstraints(balanceLabel, balanceLabelConstraints);
+        balanceJLabel.setText(BL_PLACEHOLDER);
     }
 
     @Override
-    public void displayBankAccount(BankAccount ba) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'displayBankAccount'");
+    public void displayBankAccount(final BankAccount ba) {
+        balanceJLabel.setText(Integer.toString(ba.getBalance()));
     }
-
 }
