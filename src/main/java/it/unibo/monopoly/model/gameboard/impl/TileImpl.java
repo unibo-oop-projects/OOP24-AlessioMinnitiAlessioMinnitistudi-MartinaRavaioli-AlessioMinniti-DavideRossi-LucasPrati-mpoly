8306be14dcff3fc97fd636d1c5520671ba35f4b6
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-05-08 15:46:53 +0200

    checkstyle in tile and tile impl

--- src/main/java/it/unibo/monopoly/model/gameboard/impl/TileImpl.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/TileImpl.java
@@ -4,6 +4,8 @@ import it.unibo.monopoly.model.gameboard.api.Tile;
 import it.unibo.monopoly.model.turnation.api.Position;
 import it.unibo.monopoly.resources.Identifiable;
 
-public abstract class TileImpl implements Tile,Identifiable<Position>,Comparable<TileImpl>{
-    
+/**
+ * tile implementation.
+*/
+public abstract class TileImpl implements Tile, Identifiable<Position>, Comparable<TileImpl> {
 }
