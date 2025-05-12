c5e3dbb3cdfe7aad01e5fcb2b7409ce2bac571ed
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-05-12 23:24:29 +0200

    checkstyles in identifiable

--- src/main/java/it/unibo/monopoly/resources/Identifiable.java
+++ src/main/java/it/unibo/monopoly/resources/Identifiable.java
@@ -1,8 +1,18 @@
 package it.unibo.monopoly.resources;
-
+/**
+ * identifiable interface.
+ * @param <E>
+*/
 public interface Identifiable<E> {
+    /**
+     * get ID.
+     * @return E
+    */
     E getID();
-
+    /**
+     * set ID.
+     * @param value
+    */
     void setID(E value);
 }
 
