96866e1c03eff085687ec496127f5cd96cf8879f
Lucas Prati <lucas.prati@studio.unibo.it>
2025-05-12 11:11:35 +0200

    [Tile] Modified interface Tile for support JsonTypeInfo

--- src/main/java/it/unibo/monopoly/model/gameboard/api/Tile.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/api/Tile.java
@@ -1,6 +1,21 @@
 package it.unibo.monopoly.model.gameboard.api;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import it.unibo.monopoly.model.gameboard.impl.PropertyImpl;
+import it.unibo.monopoly.model.gameboard.impl.SpecialImpl;
+
 /**
- * tile interface.
+ * Marker interface for a board tile.
  */
-public interface Tile { }
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"  // This matches "type": "PROPERTY"/"SPECIAL" in the JSON
+)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = PropertyImpl.class, name = "PROPERTY"),
+    @JsonSubTypes.Type(value = SpecialImpl.class, name = "SPECIAL")
+})
+public interface Tile {
+}
