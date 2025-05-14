7ff69940baf35a47a59afc61bd62df2bc29ba58c
Lucas Prati <lucas.prati@studio.unibo.it>
2025-05-14 16:07:45 +0200

    [Tile] added JsonType in the interface Tile, useful for the Json import in SpecialImpl or PropertyImpl

--- src/main/java/it/unibo/monopoly/model/gameboard/api/Tile.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/api/Tile.java
@@ -1,27 +1,41 @@
 package it.unibo.monopoly.model.gameboard.api;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import it.unibo.monopoly.model.gameboard.impl.PropertyImpl;
+import it.unibo.monopoly.model.gameboard.impl.SpecialImpl;
 import it.unibo.monopoly.model.gameboard.impl.Type;
 import it.unibo.monopoly.model.turnation.api.Position;
 
 /**
  * tile interface.
 */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = SpecialImpl.class, name = "SPECIAL"),
+    @JsonSubTypes.Type(value = PropertyImpl.class, name = "PROPERTY")
+})
 public interface Tile {
+
     /**
     * get the type.
     * @return Type
     */
     Type getType();
+
     /**
     * set the type.
     * @param type
     */
     void setType(Type type);
+
     /**
     * get the position.
     * @return position
     */
     Position getPosition();
+
     /**
     * get the name.
     * @return String
