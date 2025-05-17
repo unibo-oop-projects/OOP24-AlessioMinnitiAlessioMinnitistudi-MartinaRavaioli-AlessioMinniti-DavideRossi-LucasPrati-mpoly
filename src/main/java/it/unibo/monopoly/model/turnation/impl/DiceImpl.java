5043402d3d283e713339b09af290b4a6bc0f3014
Lucas Prati <lucas.prati@studio.unibo.it>
2025-05-17 14:59:14 +0200

    [Merge] merged pawn's interface, implementation and factory from development, also AbstractIdPlayer and DiceImpl

--- src/main/java/it/unibo/monopoly/model/turnation/impl/DiceImpl.java
+++ src/main/java/it/unibo/monopoly/model/turnation/impl/DiceImpl.java
@@ -1,13 +1,77 @@
 package it.unibo.monopoly.model.turnation.impl;
 
+import java.util.Collection;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import it.unibo.monopoly.model.turnation.api.Dice;
+
 
 /**
  * dice implementation.
 */
 public class DiceImpl implements Dice {
-    @Override
-    public final int throwDice() { 
-        return 0; 
+    private static final int DEFAULT_FACES = 5;
+    private final int faces;
+    private final Random rand = new Random();
+    private int nDices;
+    private final int[] dices;
+    /**
+     * constructor.
+     * @param n
+    */
+    public DiceImpl(final int n) {
+        setNDices(n);
+        this.dices = new int[this.nDices];
+        this.faces = DEFAULT_FACES;
     }
+    /**
+     * constructor.
+     * @param n
+     * @param faces
+    */
+    public DiceImpl(final int n, final int faces) {
+        setNDices(n);
+        this.dices = new int[this.nDices];
+        this.faces = faces;
+    }
+
+    /**
+     * constructor.
+    */
+    public DiceImpl() {
+        setNDices(2);
+        this.dices = new int[this.nDices];
+        this.faces = DEFAULT_FACES;
+    }
+    /**
+     * throw the dices.
+     * @return a collection of integer
+    */
+    @Override
+    public final Collection<Integer> throwDices() {
+        return IntStream.range(0, dices.length)
+                .map(i -> rand.nextInt(this.faces) + 1)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * set the number of dices.
+     * @param value
+    */
+    private void setNDices(final int value) {
+        this.nDices = value;
+    }
+
+    /**
+     * get the number of dices.
+     * @return int
+    */
+    @Override
+    public int getNDices() {
+        return this.nDices;
+    }
+
 }
