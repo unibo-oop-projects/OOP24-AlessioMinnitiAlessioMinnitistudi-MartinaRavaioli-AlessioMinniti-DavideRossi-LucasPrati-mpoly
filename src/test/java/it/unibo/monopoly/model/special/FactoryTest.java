6ba4fd44e07263d613b249a037bf237a07dcff01
Martina Ravaioli <martina.ravaioli6@studio.unibo.it>
2025-05-19 18:40:47 +0200

    writing specials test

--- src/test/java/it/unibo/monopoly/model/special/FactoryTest.java
+++ src/test/java/it/unibo/monopoly/model/special/FactoryTest.java
@@ -7,8 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
+import java.awt.Color;
+import java.util.Set;
+
+import it.unibo.monopoly.model.gameboard.api.Special;
+import it.unibo.monopoly.model.gameboard.api.SpecialFactory;
+import it.unibo.monopoly.model.gameboard.impl.SpecialFactoryImpl;
+import it.unibo.monopoly.model.turnation.api.Player;
+import it.unibo.monopoly.model.turnation.api.Position;
+import it.unibo.monopoly.model.turnation.impl.PlayerImpl;
+import it.unibo.monopoly.model.turnation.impl.PositionImpl;
 
 
 
@@ -17,15 +29,23 @@ import org.junit.jupiter.api.Test;
  * class Bank.
  */
 public class FactoryTest {
+    
+    private static final int VALID_ID = 1;
+    private static final String VALID_NAME = "Alice";
+    private static final Color VALID_COLOR = Color.GREEN;
+    private Player player;
+    private Special special;
+    private final SpecialFactory factory = new SpecialFactoryImpl(null, null, null);
+    private final Position pos = new PositionImpl(13);
 
-    @BeforeAll
+    @BeforeEach
     void create(){
-        
+        player = PlayerImpl.of(VALID_ID, VALID_NAME, VALID_COLOR);        
     }
 
     @Test
     void testPrison(){
-
+        special = factory.prison(pos);
     }
 
 }
