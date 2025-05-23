f207a22bf834525bc70f8408a24ffaa774ac331f
Martina Ravaioli <martina.ravaioli6@studio.unibo.it>
2025-05-23 21:46:27 +0200

    fixing build

--- src/main/java/it/unibo/monopoly/model/gameboard/api/EffectFactory.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/api/EffectFactory.java
@@ -2,6 +2,10 @@ package it.unibo.monopoly.model.gameboard.api;
 
 import it.unibo.monopoly.model.transactions.api.Bank;
 
+/**
+ * a factory for effects that can be used.
+ * for special, unexpected and probability tiles
+ */
 public interface EffectFactory {
 
     Effect depositMoney(int amount, Bank bank);
