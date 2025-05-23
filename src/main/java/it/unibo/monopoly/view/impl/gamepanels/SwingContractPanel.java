08744a171199920dc1c2bc0ca9b1f6a12d978090
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-05-23 14:07:41 +0200

    fix cast owner id to string

--- src/main/java/it/unibo/monopoly/view/impl/gamepanels/SwingContractPanel.java
+++ src/main/java/it/unibo/monopoly/view/impl/gamepanels/SwingContractPanel.java
@@ -84,7 +84,7 @@ final class SwingContractPanel extends SwingAbstractJPanel implements ContractPa
         final JPanel ownerPanel = new JPanel();
         ownerPanel.setLayout(new BorderLayout());
         final JLabel ownerDesc = new JLabel("Proprietario: ");
-        final JLabel ownerInfo = new JLabel(deed.isOwned() ? deed.getOwner() : "NO OWNER", 
+        final JLabel ownerInfo = new JLabel(deed.isOwned() ? Integer.toString(deed.getOwnerId()) : "NO OWNER", 
                                             SwingConstants.RIGHT);
         ownerPanel.add(ownerDesc, BorderLayout.WEST);
         ownerPanel.add(ownerInfo, BorderLayout.CENTER);
