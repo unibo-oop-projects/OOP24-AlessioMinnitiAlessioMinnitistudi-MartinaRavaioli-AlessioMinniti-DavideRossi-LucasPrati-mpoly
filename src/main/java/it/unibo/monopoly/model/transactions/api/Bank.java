7dcb6b5db591e6c2ff23d6e6e2bc7e386380cca4
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-05-24 23:20:14 +0200

    create setTurnTransactions method in Bank

--- src/main/java/it/unibo/monopoly/model/transactions/api/Bank.java
+++ src/main/java/it/unibo/monopoly/model/transactions/api/Bank.java
@@ -2,6 +2,8 @@ package it.unibo.monopoly.model.transactions.api;
 
 import java.util.Set;
 
+import it.unibo.monopoly.model.turnation.impl.PlayerImpl;
+
 /**
  * transaction manager interface.
 */
@@ -79,4 +81,22 @@ public interface Bank {
      * @param amount the amount of money to withdraw
      */
     void makePaymentToBank(int ownerId, int amount);
+
+    /**
+     * This method can be called exterally to ask the {@link Bank} which actions,
+     * the specified player, can perform on the given {@link TitleDeed}. 
+     * Additionally, it marks internally whether there are any actions that
+     * are mandatory (meaning the player has to perform to be able to end its turn,
+     * such as {@link Bank#payRent(String, int)}).
+     * @param currentPlayerId the id returned by {@link PlayerImpl#getID()}
+     * of the player that is currently playing its turn
+     * @param titleDeedName the name  of the {@link TitleDeed}
+     * associated with the {@link Property} the player's {@link Pawn}
+     * has currently stepped onto.
+     * @return a {@link Set} of {@link BankAction} that can, or have to be performed 
+     * by the player that is currently playing its turn. The {@code actions} wil be performed
+     * on the requested {@link TitleDeed}, through the {@link BankAccount} of the player whose id
+     * was given.
+     */
+    Set<BankAction> setTurnTransactions(int currentPlayerId, String titleDeedName);
 }
