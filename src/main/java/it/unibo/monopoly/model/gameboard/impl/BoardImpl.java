e336f0d0ff5519aecc357892480040d23b27b52b
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-05-24 19:21:48 +0200

    fix get effect

--- src/main/java/it/unibo/monopoly/model/gameboard/impl/BoardImpl.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/BoardImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 import it.unibo.monopoly.model.gameboard.api.Board;
 import it.unibo.monopoly.model.gameboard.api.Pawn;
 import it.unibo.monopoly.model.gameboard.api.Property;
+import it.unibo.monopoly.model.gameboard.api.Special;
 import it.unibo.monopoly.model.gameboard.api.Tile;
 import it.unibo.monopoly.model.turnation.api.Position;
 import it.unibo.monopoly.model.turnation.impl.PositionImpl;
@@ -146,7 +147,7 @@ public class BoardImpl implements Board {
                     return new PropertyImpl(t.getName(), t.getPosition(), t.getGroup());
                 } 
                 else {
-                    return new SpecialImpl(t.getName(), t.getPosition(), t.getGroup());
+                    return new SpecialImpl(t.getName(), t.getPosition(), ((Special) t).getEffect());
                 }
             }
         }
