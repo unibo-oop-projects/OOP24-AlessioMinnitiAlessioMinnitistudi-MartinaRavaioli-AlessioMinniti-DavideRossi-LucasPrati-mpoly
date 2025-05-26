0a523c733d02e9194592195306e0c61bc2d8f172
Martina Ravaioli <martina.ravaioli6@studio.unibo.it>
2025-05-26 18:11:19 +0200

    adding method to show Special tiles info

--- src/main/java/it/unibo/monopoly/view/api/ContractPanel.java
+++ src/main/java/it/unibo/monopoly/view/api/ContractPanel.java
@@ -1,5 +1,6 @@
 package it.unibo.monopoly.view.api;
 
+import it.unibo.monopoly.model.gameboard.api.Special;
 import it.unibo.monopoly.model.transactions.api.TitleDeed;
 
 /**
@@ -13,4 +14,10 @@ public interface ContractPanel extends GamePanel {
      * @param deed The {@link TitleDeed} to unpack
      */
     void displayPropertyContract(TitleDeed deed);
+
+    /**
+     * Displays information related to a {@link Special}.
+     * @param tile The {@link Special} to unpack
+     */
+    void displaySpecialInfo(Special tile);
 }
