caca7461714e6bd0f6bd84b4544c81835ec287eb
Martina Ravaioli <martina.ravaioli6@studio.unibo.it>
2025-05-30 22:47:41 +0200

    implementing deck

--- src/main/java/it/unibo/monopoly/model/gameboard/impl/chance_comunity/api/DeckCreator.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/chance_comunity/api/DeckCreator.java
@@ -2,8 +2,12 @@ package it.unibo.monopoly.model.gameboard.impl.chance_comunity.api;
 
 import java.io.FileNotFoundException;
 
+import it.unibo.monopoly.model.gameboard.api.Board;
+import it.unibo.monopoly.model.transactions.api.Bank;
+import it.unibo.monopoly.model.turnation.api.TurnationManager;
+
 public interface DeckCreator {
 
-    ChancheAndCommunityChestDeck createDeck(String type) throws FileNotFoundException;
+    ChancheAndCommunityChestDeck createDeck(String type, Board board, Bank bank, TurnationManager turnM) throws FileNotFoundException;
 
 }
--- src/main/java/it/unibo/monopoly/model/gameboard/impl/chance_comunity/impl/DeckCreatorImpl.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/chance_comunity/impl/DeckCreatorImpl.java
@@ -2,22 +2,40 @@ package it.unibo.monopoly.model.gameboard.impl.chance_comunity.impl;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.List;
 
+import it.unibo.monopoly.model.gameboard.api.Board;
 import it.unibo.monopoly.model.gameboard.impl.chance_comunity.api.ChancheAndCommunityChestDeck;
+import it.unibo.monopoly.model.gameboard.impl.chance_comunity.api.Command;
 import it.unibo.monopoly.model.gameboard.impl.chance_comunity.api.DeckCreator;
+import it.unibo.monopoly.model.transactions.api.Bank;
+import it.unibo.monopoly.model.turnation.api.TurnationManager;
 
 public class DeckCreatorImpl implements DeckCreator {
 
+
+    public DeckCreatorImpl(){
+    }
+
     @Override
-    public ChancheAndCommunityChestDeck createDeck(String type) {
+    public ChancheAndCommunityChestDeck createDeck(String type, Board board, Bank bank, TurnationManager turnM) {
         try {
             FileReader file = new FileReader("command.json");
-             
+            ParserOnHyphen paars = new ParserOnHyphen(file);
+            List<Chance_CommunityChestCard> cards = new LinkedList<>();
+            while (paars.hasNesxt()) {
+                String toInterpret = paars.next();
+                ComplexInterpreter compInt = new ComplexInterpreter(board, bank);
+                Command com = compInt.interpret(toInterpret, board, turnM);
+                cards.add(new Chance_CommunityChestCard(toInterpret, com));
+            } 
+            return new ChancheAndCommunityChestDeckImpl(cards, type);
         } catch (FileNotFoundException e) {
             System.out.println("not found");
+            return new ChancheAndCommunityChestDeckImpl(new LinkedList<>(), "null");
         }
 
-        return null;
     }
 
 }
