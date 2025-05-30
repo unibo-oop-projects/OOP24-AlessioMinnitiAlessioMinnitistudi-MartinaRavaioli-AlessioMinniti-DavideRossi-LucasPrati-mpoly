88b710fdcf4b828ddb107c44fcb2e967d89ece68
AlessioMinniti <alessio.minniti@studio.unibo.it>
2025-05-30 14:20:18 +0200

    create immutable property

---
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/ImmutableProperty.java
@@ -0,0 +1,72 @@
+package it.unibo.monopoly.model.gameboard.impl;
+
+import it.unibo.monopoly.model.gameboard.api.Property;
+import it.unibo.monopoly.model.turnation.api.Position;
+
+public class ImmutableProperty implements Property{
+
+    public ImmutableProperty(final Property prop) {
+        
+    }
+
+    @Override
+    public Group getGroup() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGroup'");
+    }
+
+    @Override
+    public void setGroup(Group group) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setGroup'");
+    }
+
+    @Override
+    public Position getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public void buildHouse() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buildHouse'");
+    }
+
+    @Override
+    public void buildHotel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buildHotel'");
+    }
+
+    @Override
+    public int getNHouses() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNHouses'");
+    }
+
+    @Override
+    public boolean hasHotel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasHotel'");
+    }
+
+    @Override
+    public boolean canBuildHouse() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'canBuildHouse'");
+    }
+
+    @Override
+    public boolean canBuildHotel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'canBuildHotel'");
+    }
+
+}
