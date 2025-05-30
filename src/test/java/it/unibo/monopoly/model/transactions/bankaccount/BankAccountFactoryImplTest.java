bd6904a5bc500eec7a65d2f195715ac09961ba37
Lucas Prati <lucas.prati@studio.unibo.it>
2025-05-30 16:54:07 +0200

    [CheckStyle] Fixed CheckStyle

--- src/test/java/it/unibo/monopoly/model/transactions/bankaccount/BankAccountFactoryImplTest.java
+++ src/test/java/it/unibo/monopoly/model/transactions/bankaccount/BankAccountFactoryImplTest.java
@@ -1,6 +1,11 @@
 package it.unibo.monopoly.model.transactions.bankaccount;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.function.Predicate;
 
