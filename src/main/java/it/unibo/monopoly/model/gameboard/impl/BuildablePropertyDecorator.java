33671acde1fafcc07f7a3a00a37bfbfcf02b47c0
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-05-31 14:02:41 +0200

    buildable property decorator impl

--- src/main/java/it/unibo/monopoly/model/gameboard/impl/BuildablePropertyDecorator.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/BuildablePropertyDecorator.java
@@ -4,4 +4,38 @@ import it.unibo.monopoly.model.gameboard.api.BuildableProperty;
 
 public class BuildablePropertyDecorator extends NormalPropertyDecorator implements BuildableProperty {
 
+    public BuildablePropertyDecorator(BuildableProperty prop) {
+        super(prop);
+    }
+
+    @Override
+    public void buildHouse() {
+        ((BuildableProperty)this.decorated).buildHouse();
+    }
+
+    @Override
+    public void buildHotel() {
+        ((BuildableProperty)this.decorated).buildHotel();
+    }
+
+    @Override
+    public int getNHouses() {
+        return ((BuildableProperty)this.decorated).getNHouses();
+    }
+
+    @Override
+    public boolean hasHotel() {
+        return ((BuildableProperty)this.decorated).hasHotel();
+    }
+
+    @Override
+    public boolean canBuildHouse() {
+        return ((BuildableProperty)this.decorated).canBuildHouse();
+    }
+
+    @Override
+    public boolean canBuildHotel() {
+        return ((BuildableProperty)this.decorated).canBuildHotel();
+    }
+
 }
