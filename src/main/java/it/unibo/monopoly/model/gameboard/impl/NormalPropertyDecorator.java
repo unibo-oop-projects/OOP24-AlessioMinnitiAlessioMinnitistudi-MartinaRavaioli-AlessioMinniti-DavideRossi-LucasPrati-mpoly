2e2e22b607acb4dd11368b707751f5f7ea9bfff0
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-05-31 13:56:34 +0200

    property decorator impl

--- src/main/java/it/unibo/monopoly/model/gameboard/impl/NormalPropertyDecorator.java
+++ src/main/java/it/unibo/monopoly/model/gameboard/impl/NormalPropertyDecorator.java
@@ -1,7 +1,38 @@
 package it.unibo.monopoly.model.gameboard.impl;
 
-import com.google.j2objc.annotations.Property;
+import it.unibo.monopoly.model.gameboard.api.Property;
+import it.unibo.monopoly.model.turnation.api.Position;
 
 public class NormalPropertyDecorator implements Property {
+    protected final Property decorated;
+
+    public NormalPropertyDecorator(final Property prop) {
+        this.decorated = prop;
+    }
+
+    @Override
+    public Group getGroup() {
+        return this.decorated.getGroup();
+    }
+
+    @Override
+    public void setGroup(Group group) {
+        this.decorated.setGroup(group);
+    }
+
+    @Override
+    public Position getPosition() {
+        return this.decorated.getPosition();
+    }
+
+    @Override
+    public String getName() {
+        return this.decorated.getName();
+    }
+
+    @Override
+    public boolean isBuildable() {
+        return this.decorated.isBuildable();
+    }
 
 }
