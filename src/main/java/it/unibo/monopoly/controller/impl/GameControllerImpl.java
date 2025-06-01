f2b9330e1bf7b6f2801b153ed96408f9735e0368
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-06-01 20:22:48 +0200

    refresh player info in start

--- src/main/java/it/unibo/monopoly/controller/impl/GameControllerImpl.java
+++ src/main/java/it/unibo/monopoly/controller/impl/GameControllerImpl.java
@@ -209,5 +209,6 @@ public final class GameControllerImpl implements GameController {
     @Override
     public void start() {
         this.turnationManager.resetBankState();
+        refreshPlayerInfo();
     }
 }
