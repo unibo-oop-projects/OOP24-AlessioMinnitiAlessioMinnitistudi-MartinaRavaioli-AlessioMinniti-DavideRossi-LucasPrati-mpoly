4b9fad9065e37ba4092cd6d77bf83c7b5cd615a5
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-06-01 12:44:36 +0200

    show list ranking and scroll panel in gui ranking

--- src/main/java/it/unibo/monopoly/view/impl/GUIRanking.java
+++ src/main/java/it/unibo/monopoly/view/impl/GUIRanking.java
@@ -2,8 +2,10 @@ package it.unibo.monopoly.view.impl;
 
 import java.util.List;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -11,8 +13,15 @@ public class GUIRanking extends JFrame {
     private static final long serialVersionUID = 1L; /**serial version UID.*/
 
     public GUIRanking(List<Pair<String, Integer>> ranks) {
-        final JPanel panel = new JPanel();
-        
+        DefaultListModel<Pair<String, Integer>> model = new DefaultListModel<>();
+
+        for (Pair<String, Integer> r : ranks) {
+            model.addElement(r);
+        }
+
+        JList<Pair<String, Integer>> list = new JList<>(model);
+        JScrollPane scrollPane = new JScrollPane(list);
+        this.add(scrollPane);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
