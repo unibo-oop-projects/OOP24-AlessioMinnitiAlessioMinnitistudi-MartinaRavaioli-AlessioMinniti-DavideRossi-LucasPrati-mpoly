8162a15a9e942bce724eaceb81a3ee0a5057318b
Davide Rossi <davide.rossi47@studio.unibo.it>
2025-06-03 10:12:54 +0200

    refactor title deed decorator

--- src/main/java/it/unibo/monopoly/model/transactions/impl/titledeed/TitleDeedWithHouses.java
+++ src/main/java/it/unibo/monopoly/model/transactions/impl/titledeed/TitleDeedWithHouses.java
@@ -5,13 +5,11 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.unibo.monopoly.model.gameboard.api.Board;
 import it.unibo.monopoly.model.gameboard.api.Property;
-import it.unibo.monopoly.model.gameboard.impl.Group;
 import it.unibo.monopoly.model.gameboard.impl.ImmutableProperty;
 import it.unibo.monopoly.model.transactions.api.RentOption;
 import it.unibo.monopoly.model.transactions.api.TitleDeed;
@@ -25,9 +23,8 @@ import it.unibo.monopoly.model.transactions.impl.rentoption.PropertyRentOptionIm
  * This {@link TitleDeed} references a {@link Property} whose name ({@link Property#getName()})
  * is the same as the {@link TitleDeed#getName()} of this deed.
  */
-public final class TitleDeedWithHouses implements TitleDeed {
+public final class TitleDeedWithHouses extends TitleDeedDecorator {
 
-    private final BaseTitleDeed decorated;
     private final Property referencedProperty;
     private final List<RentOption> propertyBasedRentOptions;
     private final Function<Integer, Integer> housePriceFunction;
@@ -36,12 +33,8 @@ public final class TitleDeedWithHouses implements TitleDeed {
     /**
      * Creates a new {@link TitleDeedWithHouses}
      * bound to a corresponding {@link Property} located in {@link Board}.
-     * @param group The Group of title deeds this deed is part of
-     * @param name The name of the deed
-     * @param salePrice The price to pay to buy the deed
-     * @param mortgageFunction The 
-     * @param baseRent The standard rent fee
-     * @param options The other rent options
+     * @param decorated the {@link TitleDeed} to decorate.
+     * @param propertyBasedRentOptions The other rent options
      * that could be applied when having to pay the rent
      * @param attachedProperty the {@link Property} to associated to this
      * deed. The deed will use it to determine the rent. {@link TitleDeed#getName()} 
@@ -61,81 +54,30 @@ public final class TitleDeedWithHouses implements TitleDeed {
                 that doesn't allow the modification of its state.
          """
     )
-    //TODO rivedere pattern decorator
-    public TitleDeedWithHouses(final Group group, 
-                        final String name, 
-                        final int salePrice, 
-                        final Function<Integer, Integer> mortgageFunction, 
-                        final int baseRent,
-                        final List<RentOption> options,
+    public TitleDeedWithHouses(
+                        final TitleDeed decorated,
+                        final List<PropertyRentOptionImpl> propertyBasedRentOptions,
                         final ImmutableProperty attachedProperty,
                         final Function<Integer, Integer> houseFunction,
                         final Function<Integer, Integer> hotelFunction) {
+        super(decorated);
         Objects.requireNonNull(hotelFunction);
         Objects.requireNonNull(houseFunction);
         Objects.requireNonNull(attachedProperty);
-        if (!name.equals(attachedProperty.getName())) {
+        if (!decorated.getName().equals(attachedProperty.getName())) {
             throw new IllegalStateException("This title deed and the associated property"
             + "must have the same name for reference purposes");
         }
-        propertyBasedRentOptions = options.stream()
-                    .filter(op -> op instanceof PropertyRentOptionImpl)
-                    .collect(Collectors.toList());
-        decorated = new BaseTitleDeed(group, name, salePrice, mortgageFunction, baseRent,
-            options
-            .stream()
-            .filter(op -> !(op instanceof PropertyRentOptionImpl))
-            .collect(Collectors.toList())
-        );
+        this.propertyBasedRentOptions = new ArrayList<>(propertyBasedRentOptions);
         this.referencedProperty = attachedProperty;
         this.housePriceFunction = houseFunction;
         this.hotelPriceFunction = hotelFunction;
     }
 
     @Override
-    public boolean isOwned() {
-        return decorated.isOwned();
-    }
-
-    @Override
-    public int getOwnerId() {
-        return decorated.getOwnerId();
-    }
-
-    @Override
-    public void setOwner(final int ownerId) {
-        decorated.setOwner(ownerId);
-    }
-
-    @Override
-    public void removeOwner() {
-        decorated.removeOwner();
-    }
-
-    @Override
-    public Group getGroup() {
-        return decorated.getGroup();
-    }
-
-    @Override
-    public String getName() {
-        return decorated.getName();
-    }
-
-    @Override
-    public Integer getSalePrice() {
-        return decorated.getSalePrice();
-    }
-
-    @Override
-    public Integer getMortgagePrice() {
-        return decorated.getMortgagePrice();
-    }
-
-    @Override
     public Integer getRent(final Set<TitleDeed> groupTitleDeeds, final int diceThrow) {
         return IntStream.concat(
-            IntStream.of(decorated.getRent(groupTitleDeeds, diceThrow)), 
+            IntStream.of(getDecorated().getRent(groupTitleDeeds, diceThrow)), 
             propertyBasedRentOptions.stream()
             .filter(op -> op.canBeApplied(groupTitleDeeds, diceThrow, referencedProperty))
             .mapToInt(RentOption::getPrice))
@@ -146,7 +88,7 @@ public final class TitleDeedWithHouses implements TitleDeed {
     @Override
     public List<RentOption> getRentOptions() {
         final List<RentOption> output = new ArrayList<>();
-        output.addAll(decorated.getRentOptions());
+        output.addAll(getDecorated().getRentOptions());
         output.addAll(propertyBasedRentOptions);
         return output;
     }
@@ -160,5 +102,4 @@ public final class TitleDeedWithHouses implements TitleDeed {
     public int getHotelPrice() {
         return hotelPriceFunction.apply(getSalePrice());
     }
-
 }
