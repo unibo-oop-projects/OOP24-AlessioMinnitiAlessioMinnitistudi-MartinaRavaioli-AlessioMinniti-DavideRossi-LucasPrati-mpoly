f810c9bfc862522eca513a4a89a2f72cde36a61a
Martina Ravaioli <martina.ravaioli6@studio.unibo.it>
2025-06-11 00:53:42 +0200

    fixing font in gui vendita

--- src/main/java/it/unibo/monopoly/view/impl/GUIVendita.java
+++ src/main/java/it/unibo/monopoly/view/impl/GUIVendita.java
@@ -4,7 +4,6 @@ package it.unibo.monopoly.view.impl;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
