41fd691dfcc48ef762ea3cc557892537e96b0ef6
Lucas Prati <lucas.prati@studio.unibo.it>
2025-06-16 00:12:25 +0200

    compete SwingMenuPanel

--- src/main/java/it/unibo/monopoly/view/api/MenuPanel.java
+++ src/main/java/it/unibo/monopoly/view/api/MenuPanel.java
@@ -5,5 +5,11 @@ package it.unibo.monopoly.view.api;
  */
 public interface MenuPanel extends GamePanel {
 
-    
+    /**
+     * Display the result of the increment/decrement of the number of players.
+     * @param num the number of player to display
+     * @param reachMinPlayers whether the number of players is equal to the minimum allowed
+     * @param reachMaxPlayers whether the number of players is equal to the maximum allowed
+     */
+    void refreshNumPlayers(final int num, final boolean reachMinPlayers, final boolean reachMaxPlayers);
 }
