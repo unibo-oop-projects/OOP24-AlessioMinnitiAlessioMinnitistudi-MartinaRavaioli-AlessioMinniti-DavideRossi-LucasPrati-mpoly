726a6c69a51099442d29704fd87d1a4917bea392
Lucas Prati <lucas.prati@studio.unibo.it>
2025-06-16 00:07:25 +0200

    [api - Controller] fix api MainMenuController

--- src/main/java/it/unibo/monopoly/controller/api/MainMenuController.java
+++ src/main/java/it/unibo/monopoly/controller/api/MainMenuController.java
@@ -1,8 +1,6 @@
 package it.unibo.monopoly.controller.api;
 
 import java.awt.Color;
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.util.Map;
 
 import it.unibo.monopoly.controller.impl.MainMenuControllerImpl;
@@ -18,9 +16,6 @@ public interface MainMenuController {
     /**
      * Must initialize all the game, starting from the given list of players'data.
      * @param playersSetup the players'data, create players according to this
-     * @throws IOException if the loading from {@code JSON} failed
-     * @throws UncheckedIOException if the parsing from {@code JSON} failed
-     * @throws NullPointerException if {@code id}, {@code name} or {@code color} are {@code null}
      */
     void onClickStart(Map<Color, String> playersSetup);
 
@@ -64,22 +59,10 @@ public interface MainMenuController {
      */
     void onClickDone();
 
-
-
     /**
      * @return the the selected number of players
      */
     int getNumPlayers();
-
-    /**
-     * @return true if the selected number of players reach the minimum, false otherwise
-     */
-    boolean alreadyMinPlayers();
-
-    /**
-     * @return true if the selected number of players reach the maximum, false otherwise
-     */
-    boolean alreadyMaxPlayers();
 
     /**
      * @return the type of bank account choosen for the game
