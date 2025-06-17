9a2ab222174bb059022072386741df2ad3716059
Alessio Minniti <alessio.minniti@studio.unibo.it>
2025-06-17 17:04:22 +0200

    add pawn iterator

---
+++ src/main/java/it/unibo/monopoly/utils/api/PawnIterator.java
@@ -0,0 +1,20 @@
+package it.unibo.monopoly.utils.api;
+
+import it.unibo.monopoly.model.gameboard.api.Pawn;
+/**
+ * iterator of pawn.
+ * it gives you the pawn with getNext.
+ * and tells you if has other pawns in it.
+ */
+public interface PawnIterator {
+    /**
+     * get you the next pawn in the iterator. 
+     * @return Pawn
+     */
+    Pawn getNext();
+    /**
+     * tells if the iterator contains other pawns.
+     * @return bool
+     */
+    boolean hasMore();
+}
